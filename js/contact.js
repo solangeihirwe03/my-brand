44033d95af40cafcc73819394844ce185a2edfc9
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-03-17 16:11:23 +0200

    new changes

--- js/contact.js
+++ js/contact.js
@@ -4,103 +4,113 @@ const contact = document.getElementById('contact');
 const email = document.getElementById('email');
 const textarea = document.getElementById('textarea');
 
-function showError(input, message){
-    var formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message
-}
-
-function showSuccess(input, message){
+function showError(input, message) {
   var formControl = input.parentElement;
-  formControl.className ='form-control success';
+  formControl.classList.add('error');
   const small = formControl.querySelector('small');
-  small.innerText = message;
+  small.innerText = message
 }
 
-
-function checkUsername(input, min, max){
-    let alphaExp = /^[a-zA-Z]+$/;
-    if (input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be at least ${min} characters long.`);
-        return false; 
-      } else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be less than ${max} characters long.`);
-        return false;
-      } else if (!alphaExp.test(input.value)) { 
-        showError(input, `${getFieldName(input)} must contain only letters, no numbers.`);
-        return false;
-      }
-      else{
-        showSuccess(input, 'success')
-      }
-      
+function showSuccess(input) {
+  var formControl = input.parentElement;
+  formControl.classList.remove('error');
+  const small = formControl.querySelector('small');
+  small.innerText = '';
 }
-function checkContact(input, min, max){
-    let regexp = /^[0-9]+?$/;
-    if(input.value.length < min){
-        showError(input, `${getFieldName(input)} must be at least ${min} numbers.`);
-        return false; 
-      } else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be less than ${max} numbers.`);
-        return false;
-      } else if (!regexp.test(input.value)) { 
-        showError(input, `${getFieldName(input)} must contain only numbers, not letters.`);
-        return false;
-      }
-    
-      else{
-        showSuccess(input, 'success')
-      }
+function clearErrors() {
+  const errorMessages = document.querySelectorAll('small');
+  errorMessages.forEach(message => message.innerText = ''); // Clear error messages
+
+  success.innerText = ''; // Clear any previous success message
 }
 
-function checkEmail(input){
-    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if(re.test(input.value.trim())){
-        showSuccess(input, 'success')
-      }
-    
-    else{
-        showError(input, 'email is not valid')
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`)
     }
+  });
 }
 
-function checkTextarea(input, min, max){
-    let letters = /^[0-9a-zA-Z]+$/;
-    if(input.value.length < min){
-        showError(input, `${getFieldName(input)} must be at least ${min} characters.`);
-        return false; 
-      } else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be less than ${max} characters.`);
-        return false;
-      } else if (!letters.test(input.value)) { 
-        showError(input, `${getFieldName(input)} must contain only numbers, not letters.`);
-        return false;
-      }
-    
-      else{
-        showSuccess(input, 'success')
-      }
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-function checkRequired(inputArr){
-    inputArr.forEach(function(input){
-        if(input.value.trim()=== ''){
-            showError(input, `${getFieldName(input)} is required`)
-        }
-    });
+function checkUsername(input) {
+  let alphaExp = /^[a-zA-Z]+$/;
+  if(input.value === ''){
+    showError(input, `${getFieldName(input)} is required`)
+  }
+  else if (input.value.length < 4) {
+    showError(input, `${getFieldName(input)} must be at least 4 characters long.`);
+    return false;
+  } else if (!alphaExp.test(input.value)) {
+    showError(input, `${getFieldName(input)} must contain only letters, no numbers.`);
+    return false;
+  }
+  else {
+    showSuccess(input)
+  }
+
+}
+function checkContact(input) {
+  let regexp = /^[0-9]+?$/;
+  if(input.value === ''){
+    showError(input, `${getFieldName(input)} is required`)
+  }
+  else if (input.value.length < 10) {
+    showError(input, `${getFieldName(input)} must be at less than 8 numbers.`);
+    return false;
+  } else if (!regexp.test(input.value)) {
+    showError(input, `${getFieldName(input)} must contain only numbers, not letters.`);
+    return false;
+  }
+
+  else {
+    showSuccess(input)
+  }
 }
 
-function getFieldName(input){
-    return input.id;
+function checkEmail(input) {
+  const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  if(input.value === ''){
+    showError(input, `${getFieldName(input)} is required`)
+  }
+  else if (re.match(input.value.trim())) {
+    showError(input, 'email is not valid')
+  }
+
+  else {
+    showSuccess(input)
+  }
 }
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    checkRequired([userName, contact, email, textarea])
-    checkUsername(userName, 4 ,20);
-    checkContact(contact, 8, 12 );
-    checkEmail(email);
-    checkTextarea(textarea, 3, 100 )
-   
+function checkTextarea(input) {
+  let letters = /^[0-9a-zA-Z]+$/;
+  if(input.value === ''){
+    return(input, `${getFieldName(input)} is required`)
+  }
+  else if(input.value.length < 4) {
+    showError(input, `${getFieldName(input)} must be at least 4 characters.`);
+    return false;
+  } else if (!letters.test(input.value)) {
+    showError(input, `${getFieldName(input)} must contain only numbers, not letters.`);
+    return false;
+  }
+
+  else {
+    showSuccess(input);
+  }
+}
+
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkRequired([userName, contact, email, textarea])
+  checkUsername(userName);
+  checkContact(contact);
+  checkEmail(email);
+  checkTextarea(textarea)
+ 
 })
